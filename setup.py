0507336d5fc8460d0b1854cc8fb9da5f2d6b7361
Max Howard <max@nanome.ai>
2020-02-03 16:49:13 -0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (pathlib.Path(__file__).parent / "README.md").read_text()
 setup(
 	name = 'nanome-structure-prep',
 	packages=find_packages(),
-	version = '0.1.0',
+	version = '0.1.1',
 	license='MIT',
 	description = 'A Nanome plugin to clean up selected structures.',
 	long_description = README,
