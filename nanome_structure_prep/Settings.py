864ffe33bb690fca0be8d5a12cc1eb312a4913c1
Astrovicis <tmaxhoward@gmail.com>
2020-02-03 16:05:44 -0800

    temporarily disabling dssp (lib needs updating)

--- nanome_structure_prep/Settings.py
+++ nanome_structure_prep/Settings.py
@@ -29,14 +29,15 @@ class Settings():
         self.__ln_btn_dssp   = self.__menu.root.find_node('DSSP Button')
         self.__img_dssp  = self.__ln_btn_dssp.create_child_node('Image')
         self.__img_dssp.forward_dist = -0.003
-        self.__img_dssp.add_new_image(ON_ICON_PATH)
+        self.__img_dssp.add_new_image(OFF_ICON_PATH)
         self.__btn_dssp = self.__ln_btn_dssp.get_content()
         self.__btn_dssp.img = self.__img_dssp
 
         # button config (dssp)
         self.__btn_dssp.outline.active = False
         self.__btn_dssp.name = self.__ln_btn_dssp.name
-        self.__btn_dssp.register_pressed_callback(self.set_option)
+        # TODO: Put back
+        # self.__btn_dssp.register_pressed_callback(self.set_option)
 
         self.use_bonds = True
         self.use_dssp  = True
