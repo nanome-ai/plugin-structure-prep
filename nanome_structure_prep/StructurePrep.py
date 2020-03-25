1361bad442bfb336d192e554c437f75d344bb4db
Ramji <jeremie.rahm@epitech.eu>
2020-03-25 13:44:23 -0700

    Add integration and add pycache to gitignore

--- nanome_structure_prep/StructurePrep.py
+++ nanome_structure_prep/StructurePrep.py
@@ -6,6 +6,7 @@ from .Settings import Settings
 class StructurePrep(nanome.PluginInstance):
     def start(self):
         self.settings = Settings(self, lambda b: None)
+        self.integration.structure_prep = self.integration_request
 
     def on_run(self):
         self.request_complex_list(self.get_complexes_deep)
@@ -16,9 +17,17 @@ class StructurePrep(nanome.PluginInstance):
             self.send_notification(nanome.util.enums.NotificationTypes.error, "Please select an entry")
             return
         self.set_plugin_list_button(self.PluginListButtonType.run, "Running...", False)
-        self.request_complexes(selected, self.step1)
+        self.request_complexes(selected, self.start_step1)
 
-    def step1(self, complex_list):
+    def integration_request(self, request):
+        def sender(complex_list):
+            request.send_response(complex_list)
+        self.step1(request.get_args(), sender)
+
+    def start_step1(self, complex_list):
+        self.step1(complex_list, self.done)
+
+    def step1(self, complex_list, sender):
         if self.settings.use_bonds:
             # remove bonds first
             for complex in complex_list:
@@ -26,17 +35,18 @@ class StructurePrep(nanome.PluginInstance):
                     atom._bonds.clear()
                 for residue in complex.residues:
                     residue._bonds.clear()
-            self.update_structures_deep(complex_list)
             # readd bonds
-            self.add_bonds(complex_list, self.step2)
+            def start_step2(complex_list):
+                self.step2(complex_list, sender)
+            self.add_bonds(complex_list, start_step2)
         else:
-            self.step2(complex_list)
+            self.step2(complex_list, sender)
 
-    def step2(self, complex_list):
+    def step2(self, complex_list, sender):
         if self.settings.use_dssp:
-            self.add_dssp(complex_list, self.done)
+            self.add_dssp(complex_list, sender)
         else:
-            self.done(complex_list)
+            sender(complex_list)
 
     def done(self, complex_list):
         self.send_notification(nanome.util.enums.NotificationTypes.success, "Structures prepped")
