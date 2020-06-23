92feee9dd2f56b7bb6f6d11ecc6f22e88eefe9b2
Alex McNerney <ajm13@me.com>
2020-06-23 11:04:34 -0700

    Fix settings toggle buttons on 1.18

--- nanome_structure_prep/Settings.py
+++ nanome_structure_prep/Settings.py
@@ -13,36 +13,38 @@ class Settings():
         self.__menu.register_closed_callback(on_close)
 
         # layout node setup (bonds)
-        self.__ln_btn_bonds   = self.__menu.root.find_node('Bonds Button')
-        self.__img_bonds  = self.__ln_btn_bonds.create_child_node('Image')
-        self.__img_bonds.forward_dist = -0.003
-        self.__img_bonds.add_new_image(ON_ICON_PATH)
-        self.__btn_bonds = self.__ln_btn_bonds.get_content()
-        self.__btn_bonds.img = self.__img_bonds
+        ln_btn_bonds = self.__menu.root.find_node('Bonds Button')
+        btn_bonds = ln_btn_bonds.get_content()
+        btn_bonds.icon.active = True
+        btn_bonds.icon.value.set_all(OFF_ICON_PATH)
+        btn_bonds.icon.value.selected = ON_ICON_PATH
+        btn_bonds.icon.value.selected_highlighted = ON_ICON_PATH
+        btn_bonds.selected = True
 
         # button config (bonds)
-        self.__btn_bonds.outline.active = False
-        self.__btn_bonds.name = self.__ln_btn_bonds.name
-        self.__btn_bonds.register_pressed_callback(self.set_option)
+        btn_bonds.outline.active = False
+        btn_bonds.name = ln_btn_bonds.name
+        btn_bonds.register_pressed_callback(self.set_option)
 
         # layout node setup (dssp)
-        self.__ln_btn_dssp   = self.__menu.root.find_node('DSSP Button')
-        self.__img_dssp  = self.__ln_btn_dssp.create_child_node('Image')
-        self.__img_dssp.forward_dist = -0.003
-        self.__img_dssp.add_new_image(ON_ICON_PATH)
-        self.__btn_dssp = self.__ln_btn_dssp.get_content()
-        self.__btn_dssp.img = self.__img_dssp
+        ln_btn_dssp = self.__menu.root.find_node('DSSP Button')
+        btn_dssp = ln_btn_dssp.get_content()
+        btn_dssp.icon.active = True
+        btn_dssp.icon.value.set_all(OFF_ICON_PATH)
+        btn_dssp.icon.value.selected = ON_ICON_PATH
+        btn_dssp.icon.value.selected_highlighted = ON_ICON_PATH
+        btn_dssp.selected = True
 
         # button config (dssp)
-        self.__btn_dssp.outline.active = False
-        self.__btn_dssp.name = self.__ln_btn_dssp.name
-        self.__btn_dssp.register_pressed_callback(self.set_option)
+        btn_dssp.outline.active = False
+        btn_dssp.name = ln_btn_dssp.name
+        btn_dssp.register_pressed_callback(self.set_option)
 
         self.use_bonds = True
-        self.use_dssp  = True
+        self.use_dssp = True
 
-        self.__btn_bonds.selected = self.use_bonds
-        self.__btn_dssp.selected = self.use_dssp
+        btn_bonds.selected = self.use_bonds
+        btn_dssp.selected = self.use_dssp
 
     def open_menu(self):
         self.__menu.enabled = True
@@ -51,7 +53,6 @@ class Settings():
 
     def set_option(self, button):
         button.selected = not button.selected
-        button.img.get_content().file_path = ON_ICON_PATH if button.selected else OFF_ICON_PATH
         option_name = 'bonds' if 'bonds' in button.name.lower() else 'dssp'
-        setattr(self, "use_"+option_name, button.selected)
-        self.__plugin.update_menu(self.__menu)
+        setattr(self, 'use_' + option_name, button.selected)
+        self.__plugin.update_content(button)
