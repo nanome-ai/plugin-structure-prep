007ca40280f1d60774bd3b189356ed3d3e4b5eee
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2022-04-05 12:29:09 -0400

    Use readable hostname on deploy (#6)

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,14 +3,16 @@
 echo "./deploy.sh $*" > redeploy.sh
 chmod +x redeploy.sh
 
-existing=$(docker ps -aq -f name=structure-prep)
+container_name=structure-prep
+existing=$(docker ps -aq -f name=$container_name)
 if [ -n "$existing" ]; then
     echo "removing existing container"
     docker rm -f $existing
 fi
 
 docker run -d \
---name structure-prep \
+--name $container_name \
 --restart unless-stopped \
+-h $(hostname)-$container_name \
 -e ARGS="$*" \
-structure-prep
+$container_name
